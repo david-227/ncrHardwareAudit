e70072622b97ccf521056195406466682b8c47c9
david-227 <davidmthomas227@gmail.com>
2021-06-09 09:03:46 -0400

    Force HWID to display in Caps

--- Powershell/PullHWID_v2.ps1
+++ Powershell/PullHWID_v2.ps1
@@ -2,6 +2,6 @@
 Set-Variable -Name "cleanedString" -Value (Get-ItemPropertyValue -path "HKLM:\SOFTWARE\WOW6432Node\NCR\VERSIONS" -Name SerialNumberOS)
 
 # convert to hex
-Set-Variable -Name "RealHWID" -Value ([Convert]::ToString($cleanedString, 16))
+Set-Variable -Name "RealHWID" -Value ([Convert]::ToString($cleanedString, 16)).toUpper()
 
 Write-Output "Hardware ID: $realHWID"
